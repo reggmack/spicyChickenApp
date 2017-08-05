78e572a5ef5e7a80e006dacf94bf2c8d8cf5a675
reggmack <thegraphixninja@gmail.com>
2017-08-05 09:36:49 -0500

    Configure Cloud
    Configure Cloud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,14 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
+
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': '75060ac2'
+  }
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -18,6 +26,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    CloudModule.forRoot(cloudSettings)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
